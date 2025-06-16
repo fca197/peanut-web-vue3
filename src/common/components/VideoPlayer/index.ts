5f560677ba7dbeaa663b0cba4ff93c378009a4af
wangbao <197235339@qq.com>
2025-06-16 09:57:24 +0800

    增加帮助视频demo

--- src/common/components/VideoPlayer/index.ts
+++ src/common/components/VideoPlayer/index.ts
@@ -1,3 +1,25 @@
-export const videoPathMap = {
-  "/dashboard": "https://solveplan.cn/upload/video/aps-home.mov"
+export interface videoPathInfo {
+  url: string
+  videoItemList: videoPathItem[]
 }
+
+export interface videoPathItem {
+  name: string
+  url: string
+}
+
+export const videoPathInfo: videoPathInfo [] = [
+  {
+    url: "/dashboard",
+    videoItemList: [
+      {
+        name: "首页文档",
+        url: "https://solveplan.cn/upload/video/aps-home.mov"
+      },
+      {
+        name: "首页文档2",
+        url: "https://solveplan.cn/upload/video/aps-home.mov"
+      }
+    ]
+  }
+]
--- src/common/components/VideoPlayer/index.vue
+++ src/common/components/VideoPlayer/index.vue
@@ -1,28 +1,37 @@
 <script lang="ts" setup>
 
 import { ref } from 'vue'
-import { useRoute } from 'vue-router';
-import { videoPathMap } from "./index.ts";
+import { useRoute } from 'vue-router'
+import { videoPathInfo, videoPathItem } from "./index.ts"
 
 const videoRef = ref(null)
 const videoSrc = ref<string>(null)
-
+const videoItemList = ref<videoPathItem[]>([])
 // 获取当前路由信息
-const route = useRoute();
+const route = useRoute()
 
-const showVideoDialog = ref<boolean>(false);
+const showVideoDialog = ref<boolean>(false)
+const showVideoItem = ref<boolean>(false)
 
 const playVideoFun = () => {
-
-  const currentPath = route.path;
+  showVideoDialog.value = false
+  showVideoItem.value = false
+  videoSrc.value = ""
+  const currentPath = route.path
   console.info("path", currentPath)
-  for (let key in videoPathMap) {
-    let videoPathMapElement = videoPathMap[key]
+  for (let key in videoPathInfo) {
+    const videoPathMapElement = videoPathInfo[key]
     console.info("key ", key, videoPathMapElement)
-    if(currentPath.startsWith(key)) {
-      videoSrc.value = videoPathMapElement
-      showVideoDialog.value = true;
-      return;
+    if(currentPath.startsWith(videoPathMapElement.url)) {
+      const videoItemListTmp = videoPathMapElement.videoItemList
+      if(videoItemListTmp.length === 1) {
+        videoSrc.value = videoItemListTmp[0].url
+        showVideoItem.value = true
+      } else {
+        videoItemList.value = videoItemListTmp
+      }
+      showVideoDialog.value = true
+      return
     }
   }
   ElMessage.warning("本页面暂时无法提供帮助视频")
@@ -37,14 +46,25 @@ const togglePlay = () => {
     video.pause()
   }
 }
+
+const playVideoItem = (item: videoPathItem) => {
+  videoSrc.value = item.url
+  console.info("playVideoItem ", videoSrc.value)
+  showVideoItem.value = true
+  // togglePlay()
+}
 </script>
 
 <template>
   <div class="notify">
     <VideoPlay @click="playVideoFun" class="svg-icon"/>
-    <el-dialog title="帮助视频" v-model="showVideoDialog" destroy-on-close :width="950">
+    <el-dialog title="帮助视频" v-model="showVideoDialog" :destroy-on-close="true" :width="950">
       <div class="video-container">
+        <div v-show="!showVideoItem">
+          <div class="videoItem" v-for="item in videoItemList" @click="playVideoItem(item)">{{ item.name }}</div>
+        </div>
         <video
+          v-if="showVideoItem"
           width="96%" height="550px"
           ref="videoRef"
           class="video-player"
@@ -75,5 +95,18 @@ const togglePlay = () => {
 
 .video-container {
   text-align: center;
+}
+
+.videoItem:before {
+  content: "-";
+  margin-right: 5px;
+}
+
+.videoItem {
+  line-height: 30px;
+  height: 30px;
+  font-size: 17px;
+  text-align: left;
+  margin-left: 10px;
 }
 </style>
