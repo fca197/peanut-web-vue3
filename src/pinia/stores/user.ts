13b13da2fbc2ac6fc929f39180504cf8c3efcc01
wangbao <197235339@qq.com>
2025-06-21 21:38:10 +0800

    权限不存在，直接登出

--- src/pinia/stores/user.ts
+++ src/pinia/stores/user.ts
@@ -1,41 +1,42 @@
-import { pinia } from "@/pinia"
-import { resetRouter } from "@/router"
-import { getCurrentUserApi, getCurrentUserResourceList } from "@@/apis/users"
-import { getToken, removeToken, setKeyValue, setToken as _setToken } from "@@/utils/cache/cookies"
-import { useSettingsStore } from "./settings"
-import { useTagsViewStore } from "./tags-view"
+import {pinia} from "@/pinia"
+import {resetRouter} from "@/router"
+import {getCurrentUserApi, getCurrentUserResourceList} from "@@/apis/users"
+import {getToken, removeToken, setKeyValue, setToken as _setToken} from "@@/utils/cache/cookies"
+import {useSettingsStore} from "./settings"
+import {useTagsViewStore} from "./tags-view"
 
 export const useUserStore = defineStore("user", () => {
   const token = ref<string>(getToken() || "")
   const roles = ref<string[]>([])
   const username = ref<string>("")
   const loginPhone = ref<string>("")
-  
+
   const tagsViewStore = useTagsViewStore()
   const settingsStore = useSettingsStore()
-  
+
   // 设置 Token
   const setToken = (value: string) => {
     _setToken(value)
     token.value = value
   }
-  
+
   // 获取用户详情
   const getUserInfo = async () => {
-    const { data } = await getCurrentUserApi()
+    const {data} = await getCurrentUserApi()
     username.value = data.userName;
     setKeyValue("userName", username.value);
     loginPhone.value = data.loginPhone
     // 验证返回的 roles 是否为一个非空数组，否则塞入一个没有任何作用的默认角色，防止路由守卫逻辑进入无限循环
     // roles.value = routerConfig.defaultRoles
   }
-  
+
   const getMenuPathList = async () => {
-    const { data } = await getCurrentUserResourceList();
+    const {data} = await getCurrentUserResourceList();
     // console.info("getMenuPathList data",data)
     roles.value = data.dataList.map(tt => tt.resourceUrl)
+    // roles.value = []
   }
-  
+
   // 模拟角色变化
   const changeRoles = (role: string) => {
     const newToken = `token-${role}`
@@ -44,7 +45,7 @@ export const useUserStore = defineStore("user", () => {
     // 用刷新页面代替重新登录
     location.reload()
   }
-  
+
   // 登出
   const logout = () => {
     removeToken()
@@ -53,23 +54,33 @@ export const useUserStore = defineStore("user", () => {
     resetRouter()
     resetTagsView()
   }
-  
+
   // 重置 Token
   const resetToken = () => {
     removeToken()
     token.value = ""
     roles.value = []
   }
-  
+
   // 重置 Visited Views 和 Cached Views
   const resetTagsView = () => {
-    if(!settingsStore.cacheTagsView) {
+    if (!settingsStore.cacheTagsView) {
       tagsViewStore.delAllVisitedViews()
       tagsViewStore.delAllCachedViews()
     }
   }
-  
-  return { token, roles, username, setToken, getUserInfo, changeRoles, logout, resetToken, getMenuPathList }
+
+  return {
+    token,
+    roles,
+    username,
+    setToken,
+    getUserInfo,
+    changeRoles,
+    logout,
+    resetToken,
+    getMenuPathList
+  }
 })
 
 /**
--- src/router/guard.ts
+++ src/router/guard.ts
@@ -1,17 +1,16 @@
-import type { Router } from "vue-router"
-import { usePermissionStore } from "@/pinia/stores/permission"
-import { useUserStore } from "@/pinia/stores/user"
-import { routerConfig } from "@/router/config"
-import { isWhiteList } from "@/router/whitelist"
-import { setRouteChange } from "@@/composables/useRouteListener"
-import { useTitle } from "@@/composables/useTitle"
-import { getToken } from "@@/utils/cache/cookies"
+import type {Router} from "vue-router"
+import {usePermissionStore} from "@/pinia/stores/permission"
+import {useUserStore} from "@/pinia/stores/user"
+import {isWhiteList} from "@/router/whitelist"
+import {setRouteChange} from "@@/composables/useRouteListener"
+import {useTitle} from "@@/composables/useTitle"
+import {getToken} from "@@/utils/cache/cookies"
 import NProgress from "nprogress"
-import { trackPageView } from "@@/utils/baiduAnalytics";
+import {trackPageView} from "@@/utils/baiduAnalytics";
 
-NProgress.configure({ showSpinner: false })
+NProgress.configure({showSpinner: false})
 
-const { setTitle } = useTitle()
+const {setTitle} = useTitle()
 
 const LOGIN_PATH = "/login"
 
@@ -22,30 +21,35 @@ export function registerNavigationGuard(router: Router) {
     const userStore = useUserStore()
     const permissionStore = usePermissionStore()
     // 如果没有登录
-    if(!getToken()) {
+    if (!getToken()) {
       // 如果在免登录的白名单中，则直接进入
-      if(isWhiteList(to)) return true
+      if (isWhiteList(to)) return true
       // 其他没有访问权限的页面将被重定向到登录页面
       return LOGIN_PATH
     }
     // 如果已经登录，并准备进入 Login 页面，则重定向到主页
-    if(to.path === LOGIN_PATH) return "/"
+    if (to.path === LOGIN_PATH) return "/"
     // 如果用户已经获得其权限角色
-    if(userStore.roles.length !== 0) return true
+    if (userStore.roles.length !== 0) return true
     // 否则要重新获取权限角色
     try {
-      
+
       await userStore.getMenuPathList()
       await userStore.getUserInfo()
       // 注意：角色必须是一个数组！ 例如: ["admin"] 或 ["developer", "editor"]
       const roles = userStore.roles
+      if (roles.length === 0) {
+        userStore.logout();
+        ElMessage.error("用户权限未配置， 请联系管理员添加")
+        return LOGIN_PATH;
+      }
       // 生成可访问的 Routes
       // routerConfig.dynamic ? permissionStore.setRoutes(roles) : permissionStore.setAllRoutes()
       permissionStore.setRoutes(roles)
       // 将 "有访问权限的动态路由" 添加到 Router 中
       permissionStore.addRoutes.forEach(route => router.addRoute(route))
       // 设置 replace: true, 因此导航将不会留下历史记录
-      return { ...to, replace: true }
+      return {...to, replace: true}
     } catch (error) {
       // 过程中发生任何错误，都直接重置 Token，并重定向到登录页面
       userStore.resetToken()
@@ -53,7 +57,7 @@ export function registerNavigationGuard(router: Router) {
       return LOGIN_PATH
     }
   })
-  
+
   // 全局后置钩子
   router.afterEach((to) => {
     trackPageView(to.fullPath)
